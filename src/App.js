5a62293fd9de3d50931d90c557594247164380ed
gavin-rilee <you@example.com>
2021-03-26 13:50:11 -0400

    Final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link, Route, Switch } from 'react-router-dom';
+import Form from "./Components/Form";
+
+
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+<Link to="/pizza"><button>Order Here!</button></Link>
+      <p>You can order if you want to or leave your money behind!</p>
+      <Switch>
+        <Route path="/pizza" component={Form}/>
+        <Route path="/"/>
+      </Switch>
     </>
   );
 };
